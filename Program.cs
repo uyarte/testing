8e8ce36f7137f50351c88aa02eba16f24cbd6c7f
Rodolfo Uyarte <rolo@Rodolfos-MacBook-Pro.local>
2020-10-11 18:50:46 -0400

    new

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
